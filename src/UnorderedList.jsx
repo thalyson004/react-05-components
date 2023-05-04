b2c759af5f6dc5b5acfa0648f9584e1d8cf9c21d
Thalyson Gomes <thalyson.uece@gmail.com>
2023-05-04 07:20:36 -0300

    Initial commit

---
+++ src/UnorderedList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function UnorderedList() {
+  return (
+    <div>
+      <ul>
+        <li>Bacon</li>
+        <li>Jamon</li>
+        <li>Noodles</li>
+      </ul>
+    </div>
+  );
+}
+
+export default UnorderedList;
